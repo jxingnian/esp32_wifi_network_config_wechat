b6c9eefc047c3926312e5e447b21604d5f010522
xingnina <j_xingnian@163.com>
2025-01-03 00:18:12 +0800

    删除错误提交代码

--- main/wifi_manager.h
+++ main/wifi_manager.h
@@ -4,14 +4,8 @@
 #include "esp_wifi.h"
 #include "esp_event.h"
 
-// WiFi事件回调函数类型
-typedef void (*wifi_connected_callback_t)(void);
-
 // WiFi初始化函数
 esp_err_t wifi_init_softap(void);
-
-// WiFi STA模式初始化函数
-esp_err_t wifi_init_sta(const char *ssid, const char *password, wifi_connected_callback_t callback);
 
 // WiFi扫描函数
 esp_err_t wifi_scan_networks(wifi_ap_record_t **ap_records, uint16_t *ap_count);
